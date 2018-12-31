0b1bf4c83d5ebd01959f2f8baecda26c1218552d
kleverson <nascimentokleverson@gmail.com>
2018-12-31 21:53:19 -0200

    modal add date

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -13,6 +13,8 @@ import todayImage from '../../assets/imgs/today.jpg';
 import commonStyles from '../commonStyles';
 import Task from '../components/Task';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ActionButton from 'react-native-action-button';
+import AddTask from './AddTasks';
 
 export default class Agenda extends Component {
     state = {
@@ -38,6 +40,19 @@ export default class Agenda extends Component {
         ],
         visibleTasks: [],
         showDoneTasks: true,
+        showAddTask: false,
+    }
+
+    addTask = task => {
+        const tasks = [...this.state.tasks];
+        tasks.push({
+            id: Math.random(),
+            desc: task.desc,
+            estimateAt: task.date,
+            doneAt: null,
+        });
+
+        this.setState({ tasks, showAddTask: false }, this.filterTasks);
     }
 
     filterTasks = () => {
@@ -73,6 +88,9 @@ export default class Agenda extends Component {
     render () {
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask}
+                    onSave={this.addTask}
+                    onCancel={() => this.setState({ showAddTask: false })} />
                 <ImageBackground source={todayImage} style={styles.background}>
                     <View style={styles.iconBar}>
                         <TouchableOpacity onPress={this.toggleFilter}>
@@ -92,6 +110,8 @@ export default class Agenda extends Component {
                         keyExtractor={item => `${item.id}`}
                         renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} />} />
                 </View>
+                <ActionButton buttonColor={commonStyles.colors.today}
+                    onPress={() => { this.setState({ showAddTask: true }) }} />
             </View>
         )
     }
